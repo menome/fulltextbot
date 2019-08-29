77b082eb406152fb33aff539aff7bad94dde5cc8
Konrad Aust <konrad.aust@gmail.com>
2019-08-29 14:48:35 -0600

    Updated page number so it's 1-indexed instead of zero-indexed.

--- app/queryBuilder.js
+++ app/queryBuilder.js
@@ -21,7 +21,7 @@ module.exports.fulltextQuery = function({uuid, fulltext, fulltextKeywords, wordc
  */
 module.exports.fulltextPageQuery = function({uuid, pageTextArray}) {
   // Because cypher sucks sometimes.
-  var pageTextArrayWithIndices = pageTextArray.map((page, idx) => ({text: page, pageno: idx}))
+  var pageTextArrayWithIndices = pageTextArray.map((page, idx) => ({text: page, pageno: idx+1}))
   var query = new Query();
   query.match("(f:Card {Uuid: $uuid})", {uuid})
   query.foreach("page IN $pages", "MERGE (f)-[:HAS_PAGE]->(p:Page:Card {PageNumber: page.pageno}) ON CREATE SET p.Uuid = apoc.create.uuid() SET p.FullText = page.text, p.Name = f.Name + ' - Page '+page.pageno", {pages: pageTextArrayWithIndices})
